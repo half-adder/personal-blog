e07520fff99012e42a87df36b56eac0e8d7fa82f
Sean Johnsen <sean.b.johnsen@gmail.com>
2025-09-03 09:55:08 -0400

    Add build script for Cloudflare Pages deployment

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install obsidian-export if not present
+if [ ! -f "bin/obsidian-export" ]; then
+    echo "Installing obsidian-export..."
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    source ~/.cargo/env
+    cargo install obsidian-export
+    ln -s ~/.cargo/bin/obsidian-export bin/obsidian-export
+fi
+
+# Convert Obsidian vault to Hugo content
+echo "Converting Obsidian vault to Hugo content..."
+rm -rf content/*
+bin/obsidian-export vault/ content/ --frontmatter always
+bin/make-index-files content/
+
+# Build Hugo site
+echo "Building Hugo site..."
+hugo --minify
